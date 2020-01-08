1bed3277a0c6a9bf6fbeee73a7a083d7c71d4165
Augusto Hidalgo <ahidalgo@unan.edu.ni>
2020-01-08 13:58:05 -0600

    Creada la primera migración para el modelo de registro.

---
+++ Proyecto_Inventario_MVC/Data/Migrations/20200108195359_Primera-Migracion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_Inventario_MVC.Data.Migrations
+{
+    public partial class PrimeraMigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
